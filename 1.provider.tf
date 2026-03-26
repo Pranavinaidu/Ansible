ba1f8e718849d5987a646b8f98e59df7ea672b46
Pranavi <pranavidevaraj05@gmail.com>
2026-03-26 16:46:07 +0530

    ansible environment code

---
+++ 1.provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 2.8.0" #Forcing which version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "ansibleterraformtfstate "
+    key            = "ansibleterraformtfstate.tfstate"
+    region         = "us-east-1"
+  }
+}
